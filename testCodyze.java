82d2f10dd4f9c3a66399bb3d6c8b20652b49c505
Yang Chen <56383562+LALAYANG@users.noreply.github.com>
2021-09-12 17:09:22 +0800

    Update testCodyze.java

--- testCodyze.java
+++ testCodyze.java
@@ -43,6 +43,3 @@ public class Arith
 		System.out.println("123.3/100 = " + Arith.div(123.3, 100));
 	}
 }
-————————————————
-版权声明：本文为CSDN博主「handler-刘」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_15371293/article/details/115523087
